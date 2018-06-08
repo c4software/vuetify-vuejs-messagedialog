417f5658e1e91d2f225c7fd943fa801eb490a56c
Valentin Brosseau <c4software@gmail.com>
2018-06-08 11:03:18 +0200

    Implement the Promise usage

---
+++ src/component/PromiseVuetifyMessageDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <messageDialog
+    :value="state.isOpen"
+    :title="state.title"
+    :text="state.text"
+    :closeText="state.closeText"
+    :closeColor="state.closeColor"
+    v-on:closeAction="emmitClose"
+  />
+</template>
+
+<script>
+  export default {
+    name: 'PromiseVuetifyMessageDialog',
+    data(){
+      return {
+        state: {
+          isOpen: false,
+          title: "",
+          text: "",
+          closeText: "",
+          closeColor: "",
+          promiseResolver: undefined,
+          promiseRejecter: undefined
+        }
+      }
+    },
+    methods: {
+      commit: function(newState) {
+        this.state = newState;
+      },
+      emmitClose: function(){
+        if(this.state.promiseResolver){
+          this.state.promiseResolver(true);
+          this.state.isOpen = false;
+        }
+      }
+    }
+  }
+</script>
